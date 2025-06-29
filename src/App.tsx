c080eaae30045b54bb93921808e855ee0f69d73b
CerqueiraG25 <gabryelgomes010@gmail.com>
2025-06-29 22:14:02 +0000

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Header } from './header'
+import './index.css'
+import { Body } from './body'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Body/>
+    </>
+  )
+}
+
+export default App
---
+++ src/body/index.tsx
@@ -0,0 +1,7 @@
+export function Body() {
+    return(
+        <div className="body">
+            <a href="#">Oi</a>
+        </div>
+    )
+}
